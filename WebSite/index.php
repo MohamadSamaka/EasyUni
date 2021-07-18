7fc05abec7ec2ef49c3924fd06f20713b94aae76
thexjoker71 <mhmdkais20@gmail.com>
2021-07-18 20:39:06 +0300

    php can now get the output 1 by 1 from the stdout from the scrapper and it prits it out on the page

--- WebSite/index.php
+++ WebSite/index.php
@@ -10,17 +10,25 @@
 </head>
 <body>
     <div id = "layout-wrapper">
-        <form action="index.php" method="POST">
+        <form action="index.php" method="GET">
             <input type="text" name="user" id="user" placeholder="Username">
             <input type="password" name="pass" id="pass" placeholder="Password"><br>
             <input type="submit" name="submit" id="submit">
         </form>
     </div>
+
+
     <?php
-    if(isset($_POST['submit'])){
-        $command = escapeshellcmd('python3 ../Python/Scrapper.py '.$_POST['user']." ".$_POST['pass']);
-        $output = shell_exec($command);
-        echo $output;
+    $arr = array();
+    if(isset($_GET['submit'])){
+        ob_implicit_flush(true);ob_end_flush();
+        $command = "python3 ../Python/Scrapper.py ".$_GET["user"]." ".$_GET["pass"];
+        $fp=popen($command,"r");
+        while (!feof($fp)) {
+            $buffer = fgets($fp, 4096);
+            echo "$buffer <br>";
+        }
+        pclose($fp);
     }
     ?>
 </body>
