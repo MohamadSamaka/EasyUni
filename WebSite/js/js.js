8fe7b40be7f0bc2b0c399b6cf8dfa0c8d3625aa8
thexjoker71 <mhmdkais20@gmail.com>
2021-07-22 16:49:44 +0300

    making http request with ajax now site won't wait for php and the python gonna be executed

--- WebSite/js/js.js
+++ WebSite/js/js.js
@@ -1,1 +1,19 @@
 // alert("hello world");
+
+var LayoutWrapper = document.querySelector("#layout-wrapper");
+var Form = document.querySelector("#login-form-wrapper > form");
+var Body = document.querySelector("body");
+Form.addEventListener("submit", Login);
+
+function Login(e){
+    e.preventDefault();
+    var user = document.querySelector("#user").value;
+    var pass = document.querySelector("#pass").value;
+    Body.classList.add("enable-slider");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","ScriptExecutor.php?user="+user+"&pass="+pass, true);
+    xhr.onload=function(){
+        console.log(this.responseText);
+    }
+    xhr.send();
+}
