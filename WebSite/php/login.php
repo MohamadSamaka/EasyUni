84a94100ffae29799b528538ed397af94a310ca9
thexjoker71 <mhmdkais20@gmail.com>
2021-07-22 16:46:50 +0300

    Empty Commit

--- WebSite/php/login.php
+++ WebSite/php/login.php
@@ -1,7 +1,7 @@
 <?php
     function login($F){
         $arr = array();
-        while(count($arr) < 2){
+        while(count($arr) < 4){
             $arr = array_filter(explode("|",file_get_contents($F)));
         }
         print_r($arr);
