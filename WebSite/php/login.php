44a9a956920f6b9da378a6d37272413a7676caf1
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-26 04:20:05 +0300

    changed it's name to Proccesses

--- WebSite/php/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function login($F){
-        $arr = array();
-        while(count($arr) < 2){
-            $arr = array_filter(explode("|",file_get_contents($F)));
-        }
-        print_r($arr);
-    }
-?>
