cd39f8d788f39e5f05b5da934b02aecab04dae3a
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-27 07:37:21 +0300

    i organized stuff more, each process now has it's own request now above all of this the proccessestreacker waits until he gets a response then he tracks the next process , if he gets a failure process he breaks

--- WebSite/js/js.js
+++ WebSite/js/js.js
@@ -1,21 +1,53 @@
-// alert("hello world");
-
 var LayoutWrapper = document.querySelector("#layout-wrapper");
 var Form = document.querySelector("#login-form-wrapper > form");
 var Body = document.querySelector("body");
-Form.addEventListener("submit", Login);
+var requests = [];
+var i;
+Form.addEventListener("submit", Submit);
 
-function Login(e){
+function Submit(e){
     e.preventDefault();
+    Body.classList.add("enable-slider");
     var user = document.querySelector("#user").value;
     var pass = document.querySelector("#pass").value;
-    var prams = "user="+user+"&pass="+pass+"&limit=2"
-    Body.classList.add("enable-slider");
+    var parms = `user=${user}&pass=${pass}`
     var xhr = new XMLHttpRequest();
     xhr.open("POST","Main.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onload=function(){
+        console.log("------------------------------");
+        console.log("SubmitFunction:")
         console.log(this.responseText);
     }
-    xhr.send(prams);
+    xhr.send(parms);
+
+    ProccessesTracker();
+}
+
+async function ProccessesTracker(){
+    for(i = 1; i <= 2; i++){
+        try{
+            var response = await RequestsMaker();
+            console.log(response[1] + ':');
+            console.log(Boolean(parseInt(response[0])));
+            if(!Boolean(parseInt(response[0])))
+                break;
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+
+function RequestsMaker() {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST","ProcessTracker.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload=function(){
+            console.log("------------------------------");
+            var result = this.responseText.trim().split(' ');
+            resolve(result);
+        }
+        xhr.send(`id=${i}`);
+    })
 }
