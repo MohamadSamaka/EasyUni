d015026e4ebd3a5d159d99b3c9597e94f9f09ea0
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-27 07:59:50 +0300

    just echoing something imformative

--- WebSite/php/ScriptExecutor.php
+++ WebSite/php/ScriptExecutor.php
@@ -1,10 +1,9 @@
 <?php
-    $Fname = "Output.text";
-    $Limit = $_POST['limit'];
     if(PHP_OS == "Linux")
         system('python3 ../python/Scrapper.py '.$_POST['user']. ' '. $_POST["pass"].
         ' > '.$Fname. ' 2>&1 &');
     else
         pclose(popen('start /B python3 ../python/Scrapper.py '.$_POST['user']. ' '. $_POST["pass"].
         ' > '.$Fname, "r"));
+    echo "[+] Submit\n";
 ?>
