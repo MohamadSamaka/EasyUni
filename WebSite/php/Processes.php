5109dea4fcd053ac48f34cbebb532f3b15e4d0f7
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-27 08:02:53 +0300

    increased the limit of request

--- WebSite/php/Processes.php
+++ WebSite/php/Processes.php
@@ -1,9 +1,10 @@
 <?php
+    set_time_limit(35); #makes the program wait more before throwing time error
     function Process($F, $count, $StartIndex){ #bool function
         $info = array();
-        while(count($info) < $count){ #keeps looping until it gets the process and its result
+        while(true){ #keeps looping until it gets the process and its result
             $info = array_filter(explode("|",file_get_contents($F)));
-             if(count($info) == $count)
+             if(count($info) >= $count)
                 return PassedOrNot($info, $StartIndex);
             }
     } 
