00494c12064d33019566285092c39dc1bfaf3377
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-27 07:44:01 +0300

    Revert "i organized stuff more, each process now has it's own request, i been trying to figure out why tf i made these commented functions or idk...., i think i was overthinking not sure yet"
    This reverts commit 6eb55052cbd2b5a4ff32a76bd4ef09432eb2389c.

--- WebSite/php/ProcessTracker.php
+++ WebSite/php/ProcessTracker.php
@@ -1,43 +1,22 @@
 <?php
-    require('Processes.php');
-?>
-
-<?php
     #loops the number of times of process and cheks each process
-    #the i var stands for the number of processes and its the start point
+    #the i var stands for the number of processes
     #the j var stands for the start head index
-    // for($i = $StartPoint, $j = 2; $i < $Limit; $i++, $j+=2){
-    //     $r = Process($Fname, $j, $j - 2);
-    //     Report($r, $i);
-    //     if($r)
-    //         die;
-    // }
-    $Fname = "Output.text";
-    $CheckId;
-    if(isset($_POST['id'])){
-        $CheckId =  (int)$_POST['id'];
-        Track($Fname ,$CheckId);
-    }
-?>
-
-<?php
-    function Track($Fname, $CheckId){
-        $count = $CheckId * 2; #the number of elemnts supposed to be in the array before target element appears
-        $r = Process($Fname, $count, $count - 2);
-        Report($r, $CheckId - 1);
+    for($i = 0, $j = 2; $i < $Limit; $i++, $j+=2){
+        $r = Process($Fname, $j, $j - 2);
+        Report($r, $i);
         if($r)
             die;
-            
     }
 ?>
 
 <?php
-    function Report($b, $i){ #for testing for now no more
+    function Report($b, $i){ #fot testing for now no more
         $Proccesses = array("Connection", "Login", "Token", "Navigating", "SemestersFinding", "ObtainingData");
         $r = "";
-        if($b) $r.= "0 ";
-        else $r .= "1 ";
+        if($b) $r .= "[-] ";
+        else $r .= "[+] ";
         $r .= $Proccesses[$i];
-        echo $r;
+        echo $r."\n";
     }
 ?>
