51cd65c48fef55671aa17a73af07c51d84c3e75d
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-28 07:21:19 +0300

    added simple report on login (temporary), the site is capable of informing the user of what's goin on, imma clean up later

--- WebSite/js/js.js
+++ WebSite/js/js.js
@@ -1,13 +1,19 @@
 var LayoutWrapper = document.querySelector("#layout-wrapper");
 var Form = document.querySelector("#login-form-wrapper > form");
 var Body = document.querySelector("body");
-var requests = [];
+var ReportWrapper = document.getElementById("report");
+var Report = document.querySelector("#report > div > span:nth-child(1)");
 var i;
+
 Form.addEventListener("submit", Submit);
 
 function Submit(e){
     e.preventDefault();
-    Body.classList.add("enable-slider");
+    if(!window.navigator.onLine){
+        alert("[-] Check Your Connection");
+        return;
+    }
+    EnableSlider()
     var user = document.querySelector("#user").value;
     var pass = document.querySelector("#pass").value;
     var parms = `user=${user}&pass=${pass}`
@@ -25,13 +31,23 @@ function Submit(e){
 }
 
 async function ProccessesTracker(){
-    for(i = 1; i <= 2; i++){
+    var process = ["Connect", "Login In", "Get The Token", "Navigate",
+                    "Get Your Semesters", "Collect Data"];
+    for(i = 1; i <= 6; i++){
         try{
+            Report.innerText = "Trying To " + process[i - 1];
             var response = await RequestsMaker();
             console.log(response[1] + ':');
-            console.log(Boolean(parseInt(response[0])));
-            if(!Boolean(parseInt(response[0])))
-                break;
+            if(!Boolean(parseInt(response[0]))){
+                DisableSlider();
+                if (i == 2)
+                    alert("LogginFailed");
+                ReportWrapper.style.display = "none";
+            }
+            else{
+                ReportWrapper.style.display = "block";
+                Report.innerText = response[1];
+            }
         }catch(err){
             console.log(err);
         }
@@ -45,9 +61,21 @@ function RequestsMaker() {
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onload=function(){
             console.log("------------------------------");
-            var result = this.responseText.trim().split(' ');
+            var result = this.responseText.trim().split('|');
             resolve(result);
         }
         xhr.send(`id=${i}`);
     })
+}
+
+function EnableSlider(){
+    if(Body.classList.contains("disable-slider"))
+        Body.classList.remove("disable-slider");
+    Body.classList.add("enable-slider");
+}
+
+function DisableSlider(){
+    if(Body.classList.contains("enable-slider"))
+        Body.classList.remove("enable-slider");
+    Body.classList.add("disable-slider");
 }
