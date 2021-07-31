ce9339105a9a12abd4a80ce981c17a1694917f52
thexjoker-desktop <mhmdkais20@gmail.com>
2021-07-31 09:03:31 +0300

    changed the file path no more

--- WebSite/php/ScriptExecutor.php
+++ WebSite/php/ScriptExecutor.php
@@ -1,8 +1,8 @@
 <?php
     if(PHP_OS == "Linux")
         system('python3 ../python/Scrapper.py '.$_POST['user']. ' '. $_POST["pass"].
-        ' > '.$Fname. ' 2>&1 &');
+        ' > ../'.$Fname. ' 2>&1 &');
     else
         pclose(popen('start /B python3 ../python/Scrapper.py '.$_POST['user']. ' '. $_POST["pass"].
-        ' > '.$Fname, "r"));
+        ' > ../'.$Fname, "r"));
 ?>
