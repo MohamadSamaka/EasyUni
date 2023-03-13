09fe0dc04fc7a0c290a97e5f3877f7b1a7f001db
thexjoker-desktop <mhmdkais20@gmail.com>
2023-03-14 01:53:44 +0200

    adapting to the new version of selenuim.

--- WebSite/python/Scrapper.py
+++ WebSite/python/Scrapper.py
@@ -1,5 +1,6 @@
 import chromedriver_autoinstaller
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -33,17 +34,19 @@ class Scrapper:
 
     def LinkAndCheckDriverCompatibility(self):
         options = Options()
-        options.headless = True
         options.add_argument("--remote-debugging-port=9230")
+        # options.add_argument("--headless")
         self.driver = None
         # while self.driver is None:
         try:
+            chrome_service = None
             if platform.system() == "Windows":
                 self.platform = 1
-                self.driver = webdriver.Chrome(executable_path="..\..\Attachments\chromedriver.exe", options=options)#moved to windows this  is needed to work
+                chrome_service = Service("..\..\Attachments\chromedriver.exe")#moved to windows this  is needed to work
             else:
                 self.platform = 0
-                self.driver = webdriver.Chrome("../../Attachments/chromedriver", options=options)
+                chrome_service = Service("../../Attachments/chromedriver")
+            self.driver = webdriver.Chrome(service=chrome_service, options=options)
         except SessionNotCreatedException:
             print("bad version")
             self.WebdriverUpdater()
@@ -77,12 +80,12 @@ class Scrapper:
 
     def Login(self, username, password):
         #getting the 2 inputs container, this way i won't have an error if they changed the id
-        UserAndPassContainer = self.driver.find_elements_by_class_name("md-inputfield")
-        UsernameBar = UserAndPassContainer[0].find_element_by_tag_name("input") #getting user's bar
-        PasswordBar = UserAndPassContainer[1].find_element_by_tag_name("input") #getting password's bar
+        UserAndPassContainer = self.driver.find_elements(By.CLASS_NAME, "md-inputfield")
+        UsernameBar = UserAndPassContainer[0].find_element(By.TAG_NAME,"input") #getting user's bar
+        PasswordBar = UserAndPassContainer[1].find_element(By.TAG_NAME,"input") #getting password's bar
         UsernameBar.send_keys(username) #sending the name
         PasswordBar.send_keys(password) #sending the pass
-        self.driver.find_element_by_xpath("/html/body/div[1]/form/div/div/div[5]/button/span[2]").click() #logging in
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/form/div/div/div[5]/button/span[2]").click() #logging in
         try: #catches an error which triggered due error in connection
             WebDriverWait(self.driver, 1).until(EC.visibility_of_element_located((By.CLASS_NAME, "ui-growl-item")))
             Report("|-|")
@@ -96,7 +99,7 @@ class Scrapper:
         #getting the url which has the token in it at the end vvvvv
         try:
             xpath = "/html/body/div[1]/div[2]/div/div/div[1]/div[2]/div/div[1]/form/ul/li[5]/ul/li[1]"
-            href = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, xpath))).find_element_by_tag_name("a").get_attribute("href")
+            href = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, xpath))).find_element(By.TAG_NAME,"a").get_attribute("href")
             self.token = href.partition("Token=")[2] #extracting the token from that url
         except TimeoutException:
             Report("|-|")
@@ -118,7 +121,7 @@ class Scrapper:
     def GetSemestersLabels(self): #Getting all related schedual info
         #geting the options
         try:
-            SemestersLi = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents:semesters_items"]'))).find_elements_by_tag_name("li")
+            SemestersLi = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents:semesters_items"]'))).find_elements(By.TAG_NAME, "li")
             for li in SemestersLi[1:]: #loops in the options + excluding the first useless option
                 self.SemesterLabels.append(li.get_attribute("innerHTML"))
         except TimeoutException:
@@ -132,18 +135,18 @@ class Scrapper:
             for i in range(1, len(self.SemesterLabels) + 1): #ignores the "please select"(starts counting from 1 not from 0) option but this gonna cause ignoring 1 semester so you add 1
                 ScheduleRows = [] # stores all the rows of the schedule
                 #gets the schedual's labels buttons VVVVVVV
-                SemesterBtn = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#contents\:semesters_items"))).find_elements_by_tag_name("li")[i]
+                SemesterBtn = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#contents\:semesters_items"))).find_elements(By.TAG_NAME, "li")[i]
                 self.driver.execute_script("arguments[0].click()", SemesterBtn)# clicks on the button, used this way cuz the elements are invisible
                 #waits until the schedual loads VVVV                           #if i didn't use this method i should have click on the menu to make them show then click on the label
                 WebDriverWait(self.driver, 3).until(EC.invisibility_of_element_located((By.CLASS_NAME, "j_idt323_modal")))
                 time.sleep(1) #sleeps to let the rows load 100%
                 #extracts the rows of data
-                SemesterDataRows = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/form/div/div/div[2]/div/div/div[1]/div[2]/table/tbody"))).find_elements_by_tag_name("tr")
+                SemesterDataRows = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/form/div/div/div[2]/div/div/div[1]/div[2]/table/tbody"))).find_elements(By.TAG_NAME, "tr")
                 for PieceOfData in SemesterDataRows: #loops through these rows
                     DataRow = []
-                    for RawDataContainer in PieceOfData.find_elements_by_tag_name("td")[:-2]:#loops through these pieces of data but ignores the last 2
+                    for RawDataContainer in PieceOfData.find_elements(By.TAG_NAME,"td")[:-2]:#loops through these pieces of data but ignores the last 2
                         #extracts the raw data from the label and removes the \n from it VVVVV
-                        RawData = RawDataContainer.find_elements_by_class_name("ui-widget")[0].get_attribute("innerHTML")
+                        RawData = RawDataContainer.find_elements(By.CLASS_NAME, "ui-widget")[0].get_attribute("innerHTML")
                         RawData = Replacer(RawData)
                         DataRow.append(RawData) #appends it to the current row
                     DataRow[0],DataRow[1] = DataRow[1],DataRow[0] #swaps first element with the second one
